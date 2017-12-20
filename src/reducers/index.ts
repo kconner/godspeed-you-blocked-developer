905865aae8cd84bb068373095ff1979cf2c928e9
Kevin Conner <connerk@gmail.com>
2017-12-20 00:22:36 -0500

    Make reducers saner

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,5 +1,5 @@
 import { Action } from '../actions';
-import { StoreState } from '../types/index';
+import { StoreState, Plan, Task } from '../types/index';
 import { SET_CURRENT_PLAN_ID, SET_TASK_TITLE } from '../constants/index';
 
 export default (state: StoreState, action: Action): StoreState => {
@@ -7,18 +7,9 @@ export default (state: StoreState, action: Action): StoreState => {
     case SET_CURRENT_PLAN_ID:
       return { ...state, currentPlanID: action.planID };
     case SET_TASK_TITLE:
-      // TODO: Is there a way I make this update code simpler?
-      // Maybe study the WT website's reducers for better patterns
-      const { currentPlanID: planID, plans } = state;
-      const { taskID, title } = action;
-      const plan = plans[planID];
+      const { currentPlanID, plans } = state;
+      const plan = plans[currentPlanID];
       if (!plan) {
-        return state;
-      }
-
-      const { tasks } = plan;
-      const task = tasks[taskID];
-      if (!task) {
         return state;
       }
 
@@ -26,19 +17,42 @@ export default (state: StoreState, action: Action): StoreState => {
         ...state,
         plans: {
           ...plans,
-          [planID]: {
-            ...plan,
-            tasks: {
-              ...plan.tasks,
-              [taskID]: {
-                ...task,
-                title
-              }
-            }
-          }
+          [currentPlanID]: reducePlan(plan, action)
         }
-      }
+      };
     default:
       return state;
+  }
+};
+
+const reducePlan = (plan: Plan, action: Action): Plan => {
+  switch (action.type) {
+    case SET_TASK_TITLE:
+      const { taskID } = action;
+      const { tasks } = plan;
+      const task = tasks[taskID];
+      if (!task) {
+        return plan;
+      }
+
+      return {
+        ...plan,
+        tasks: {
+          ...plan.tasks,
+          [taskID]: reduceTask(task, action)
+        }
+      };
+    default:
+      return plan;
+  }
+};
+
+const reduceTask = (task: Task, action: Action): Task => {
+  switch (action.type) {
+    case SET_TASK_TITLE:
+      const { title } = action;
+      return { ...task, title };
+    default:
+      return task;
   }
 };
