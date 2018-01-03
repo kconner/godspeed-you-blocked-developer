75f99d03e779185529a8f1afcd712f7bf3a98b99
Kevin Conner <connerk@gmail.com>
2018-01-03 15:35:38 -0500

    Group imports by mutual relevance

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
-import './index.css';
-import { Provider } from 'react-redux';
+
 import { createStore } from 'redux';
 import { StoreState } from './types/index';
 import reduce from './reducers/index';
+
+import { Provider } from 'react-redux';
+import App from './containers/App';
+import './index.css';
 
 const store = createStore<StoreState>(
   reduce,
